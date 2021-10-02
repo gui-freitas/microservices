f84dbdf728a92c5d8f5606d8bf42c5d6e5cf84db
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-10-02 17:28:51 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/com/gapp/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gapp.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
